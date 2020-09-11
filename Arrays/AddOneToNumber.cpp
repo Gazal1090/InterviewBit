2d82e3878c8aa5888d051c958c39942713f52917
Gazal1090 <55214244+Gazal1090@users.noreply.github.com>
2020-09-12 02:34:14 +0530

    Google Microsoft

---
+++ Arrays/AddOneToNumber.cpp
@@ -0,0 +1,34 @@
+// https://www.interviewbit.com/problems/add-one-to-number/
+
+ int carry = 1,i;
+    for( i = A.size()-1; i >= 0 ; i--){
+        
+        A[i] += carry;
+        carry = A[i]/10;
+        
+        if( A[i] < 10)
+            break;
+        A[i] = A[i]%10;
+    }
+    
+    while(carry){
+        
+        if(i > -1){
+            A[i] += carry;
+            carry = A[i]/10;
+            A[i] = A[i]%10;
+        }
+        
+        else{
+            A.insert(A.begin(),1);
+            return A;
+        }
+    }
+    // to avoid initial zeroes
+    i = 0;
+    while( A[i] == 0){
+        i++;
+        A.erase(A.begin());
+    }
+    return A;
+}
