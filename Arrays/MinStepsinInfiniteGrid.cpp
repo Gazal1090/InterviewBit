85a70fe612dad250f7376651ddd58a81eb7a6fc9
Gazal1090 <55214244+Gazal1090@users.noreply.github.com>
2020-09-12 02:30:51 +0530

    Easy

--- Arrays/MinStepsinInfiniteGrid.cpp
+++ Arrays/MinStepsinInfiniteGrid.cpp
@@ -1,3 +1,5 @@
+// https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
+
 int Solution::coverPoints(vector<int> &A, vector<int> &B) {
     
     int ans = 0;
@@ -6,3 +8,5 @@ int Solution::coverPoints(vector<int> &A, vector<int> &B) {
     
     return ans;
 }
+
+// O(n) solution
