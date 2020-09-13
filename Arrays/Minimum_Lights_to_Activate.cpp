72650b926269f537431b8841b086cc9fa6c1a185
Gazal Arora <55214244+Gazal1090@users.noreply.github.com>
2020-09-13 16:24:23 +0530

    Update Minimum_Lights_to_Activate.cpp

--- Arrays/Minimum_Lights_to_Activate.cpp
+++ Arrays/Minimum_Lights_to_Activate.cpp
@@ -3,26 +3,26 @@ O(N) solution
 
 int Solution::solve(vector<int> &A, int B) {
     
-    int i = 0, n = A.size(), count = 0, flag = 0, t = 0;
+    int right = 0, n = A.size(), count = 0, flag = 0, left = 0;
     
-    while( i < n){
+    while( right < n){
         
         flag = 0;
-        for( int x = i+B-1 ; x >= t ; x--){
+        for( int x = right+B-1 ; x >= left ; x--){
             
             if( A[x] == 1){
                 
-                t = x+1;
+                left = x+1;
                 flag = 1;
                 count++;
-                i = x+B; // All bulbs beyond i has light
+                right = x+B; // All bulbs beyond i has light
                 break;
             }
         }
         if( !flag)
             return -1;
         
-        if( i >= n)
+        if( right >= n)
             return count;
     }
     return count;
